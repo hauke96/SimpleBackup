2658c0d8c72d5bef02a9ed36eda6ba88fac0bd4b
Hauke Stieler <mail@hauke-stieler.de>
2015-01-22 08:46:51 +0100

    Fixed StackOverFlow-Bug in entry-list

--- SimpleBackup/Form_MainForm.cs
+++ SimpleBackup/Form_MainForm.cs
@@ -409,8 +409,7 @@ namespace SimpleBackup
                     SelectedLanguage = Convert.ToInt32(_t[5]);
                     CheckBox_ShutDown.Checked = Convert.ToBoolean(_t[6]);
 
-                    if (_t[5] == "0") ChangeLanguage("Deutsch");
-                    if (_t[5] == "1") ChangeLanguage("English");
+                    ChangeLanguage(SelectedLanguage);
 
                     ListBox_ListOfSettings.SetSelected(_i, true);
                 }
