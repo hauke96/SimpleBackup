316e5dfee533ac5b59cff19cd6481cce5c291dae
hauke96 <mail@hauke-stieler.de>
2015-12-26 19:15:37 +0100

    Help-window can be closed via button click.

--- SimpleBackup_golang/main/SimpleBackupUI.go
+++ SimpleBackup_golang/main/SimpleBackupUI.go
@@ -103,9 +103,20 @@ func (ui *MainUI) createMenuBar(layout *gtk.Layout) {
 		popup.SetTypeHint(gdk.WINDOW_TYPE_HINT_MENU)
 		popup.SetResizable(false)
 		popup.SetTitle("Nothing here :(")
-		popup.SetSizeRequest(350, 150)
-		paned := gtk.NewVBox(false, 0)
-		paned.Add(gtk.NewLabel("N0thing's here yet :(\n\nIn the end this window will show some 'about' information.\n\n"))
+		popup.SetSizeRequest(450, 150)
+		paned := gtk.NewLayout(nil, nil)
+
+		paned.Put(gtk.NewLabel("N0thing's here yet :(\n\nIn the end this window will show some 'about' information.\n\n"), 10, 10)
+
+		button := gtk.NewButtonWithLabel("OK")
+		buttonWidth := 100
+		button.SetSizeRequest(buttonWidth, 25)
+		button.Connect("button-press-event", func() { popup.Destroy() })
+		var width int
+		var height int
+		popup.GetSize(&width, &height)
+		paned.Put(button, width/2-buttonWidth/2, height-35)
+
 		popup.Add(paned)
 		popup.ShowAll()
 	})
